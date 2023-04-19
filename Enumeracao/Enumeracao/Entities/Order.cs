9fa2fc302f8750323e3a7575e9b5dcd9aa2ebc9a
Leonardo Pacheco Dos Santos <l.pacheco@globant.com>
2023-04-19 17:21:23 -0300

    enum

---
+++ Enumeracao/Enumeracao/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using Enumeracao.Entities.Enums;
+using System;
+
+namespace Enumeracao.Entities {
+    internal class Order {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString() {
+            return $"{Id} {Moment} {Status}";
+        }
+    }
+}
